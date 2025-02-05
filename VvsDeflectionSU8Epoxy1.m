0859aaaef0ea4ff476ab962a672f6995cb0874e7
Oliver Gough <123779859+o1ivez@users.noreply.github.com>
2025-02-05 11:49:39 -0700

    Update VvsDeflectionSU8Epoxy1.m

--- VvsDeflectionSU8Epoxy1.m
+++ VvsDeflectionSU8Epoxy1.m
@@ -1,11 +1,35 @@
-Voltages = [0 22 46 92 144 190 236 286 332 380];
-NumOfFringes = [0 0 0.5 1 3 4 6 9 18 22];
-Deflections = NumOfFringes.*(633/2);
-VvsD = [Voltages ; Deflections];
+%WGP Research Voltage vs Deflection code Oliver Gough
+
+%-----------------------------------------------------------------
+%Formatting Data
+
+%Voltage vs deflection measuremnts for first epoxy film
+Epoxy_1_Voltages = [0 22 46 92 144 190 236 286 332 380];
+Epoxy_1_NumOfFringes = [0 0 0.5 1 3 4 6 9 18 22];
+Epoxy_1_Deflections = Epoxy_1_NumOfFringes.*(633/2);
+Epoxy_1_VvsD = [Epoxy_1_Voltages ; Epoxy_1_Deflections];
+
+
+%-----------------------------------------------------------------
+%Code to further compare data
+
+
+%-----------------------------------------------------------------
+%Plot The Different deflections on seperate subplots
+
+%Plot data for first epoxy film
+figure(1);
+subplot(2,2,1);
 hold on
-title("Voltage vs Deflection SU8 Epoxy 1")
-ylabel("Deflection (nm)")
-xlabel("Voltage Frop Across Device")
-scatter(VvsD(1,:),VvsD(2,:))
+title('SU8 Epoxy 1','FontSize',8);
+ylabel('Deflection (nm)');
+xlabel('Voltage Drop Across Device');
+scatter(Epoxy_1_VvsD(1,:),Epoxy_1_VvsD(2,:),10,"filled");
 hold off
-legend("Data")
+
+sgtitle('Voltage vs Deflection Of Deformable Membraines');
+
+%-----------------------------------------------------------------
+%Plots Data on one big plot
+
+%figure(2);
