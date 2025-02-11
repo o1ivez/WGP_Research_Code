012d7246c5cf21e72000eea9724a6cb5609b9e80
Oliver Gough <123779859+o1ivez@users.noreply.github.com>
2025-02-11 16:59:13 -0700

    Update VvsDeflection.m

--- Version2/VvsDeflection.m
+++ Version2/VvsDeflection.m
@@ -9,12 +9,15 @@ Epoxy1      = membraneData(:, i:i+2); i =i+3;
 Epoxy1Test2 = membraneData(:, i:i+2); i =i+3;
 Grating1    = membraneData(:, i:i+2); i =i+3;
 KMSF1       = membraneData(:, i:i+2); i =i+3;
+KMSF2       = membraneData(:, i:i+2); i =i+3;
 
 %Convert the # of fringes to nm deflection TODO add more analisis
-Epoxy1      = analizeData(Epoxy1);
-Epoxy1Test2 = analizeData(Epoxy1Test2);
-Grating1    = analizeData(Grating1);
-KMSF1       = analizeData(KMSF1);
+[Epoxy1, E1avgGapSize, E1StressFit, E1Force] = analizeData(Epoxy1);
+[Epoxy1Test2, E1T2avgGapSize, E1T2StressFit, E1T2Force] = analizeData(Epoxy1Test2);
+[Grating1, G1avgGapSize, G1StressFit, G1Force] = analizeData(Grating1);
+[KMSF1, K1avgGapSize, K1StressFit, K1Force]        = analizeData(KMSF1);
+[KMSF2, K2avgGapSize, K2StressFit, K2Force]        = analizeData(KMSF2);
+
 
 %-----------------------------------------------------------------
 %Plot The Different deflections on sperate plots
@@ -31,6 +34,8 @@ ylabel('Deflection (nm)'); xlabel('Voltage Drop Across Device [V]'); hold off;
 subplot(324); hold on;
 scatter(KMSF1(:,1),KMSF1(:,2),10, "Filled"); title('KMSF 1','FontSize',8);
 ylabel('Deflection (nm)'); xlabel('Voltage Drop Across Device [V]'); hold off;
+
+%-----------------------------------------------------------------
 %Plot all on same plot
 figure(2); hold on;
 plot(Epoxy1(:,1),Epoxy1(:,2)); plot(Epoxy1Test2(:,1),Epoxy1Test2(:,2)); 
@@ -39,7 +44,25 @@ ylabel("Membraine Defelection [nm]"); xlabel("Voltage across Devices [Volts]");
 legend("SU8 Membrane Test 1","SU8 Membrane Test 2", "SU8 Grating", "KMSF Mebraine 1", Location="northwest");
 title('Voltage Vs Deflection Cummilitve Graph'); hold off;
 
+%-----------------------------------------------------------------
+%Plot deflection and stress data and maybe force
+%Code to plot bead gap size
+figure(3); hold on;
+x = ["Epoxy 1","SU8 Grating","KMSF 1", "KMSF2"];
+y = [E1avgGapSize, G1avgGapSize, K1avgGapSize, K2avgGapSize];
+subplot(311); bar(x,y); ylabel("Average Gap Size (uM)"); 
+title("Average Gap Size"); hold off;
 
+%Code to plot poly fit of stress data
+subplot(312); hold on; ylabel("Stress");
+xlabel("Strain??"); title("Membrane Stress");
+plot(E1StressFit); plot(E1T2StressFit); plot(G1StressFit); plot(K1StressFit); plot(K2StressFit);
+legend("SU8 Membrane Test 1","SU8 Membrane Test 2", "SU8 Grating", "KMSF Mebraine 1", Location="northwest");
+hold off;
 
-
-
+%Code to plot electrostatic force vs deflection
+subplot(313); hold on; xlabel("Electrostatic Force"); ylabel("Deflection");
+title("Electrostatic Force vs Deflection (nM)");
+plot(E1Force,Epoxy1); plot(E1T2Force,Epoxy1Test2); plot(G1Force, Grating1); plot(K1Force,KMSF1);
+legend("SU8 Membrane Test 1","SU8 Membrane Test 2", "SU8 Grating", "KMSF Mebraine 1", Location="northeast");
+hold off;
